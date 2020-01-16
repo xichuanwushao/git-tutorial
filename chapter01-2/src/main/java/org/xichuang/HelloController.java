b91aa3d601f71b6a4bf29512d37715786bb99f17
xichuanwushao <avatarwx@163.com>
2020-01-16 23:41:09 +0800

    使用idea创建的第一个sb项目,(打包报错解决)
    springboot打包成功

---
+++ chapter01-2/src/main/java/org/xichuang/HelloController.java
@@ -0,0 +1,27 @@
+package org.xichuang;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+//http://localhost:8081/chapter02/hello
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot!";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
+
+    @GetMapping("/hello2")
+    public String hello(HttpSession session) {
+       int maxInactiveInterval = session.getMaxInactiveInterval();
+       return String.valueOf(maxInactiveInterval);
+        //return "hello https!";
+    }
+}
---
+++ chapter01-2/src/test/java/org/xichuang/Chapter012ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter012ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
