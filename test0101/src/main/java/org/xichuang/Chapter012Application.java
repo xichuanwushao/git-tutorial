5e0b2e461325f6aa93153ae9d91bc34f3f206d7d
xichuanwushao <avatarwx@163.com>
2020-01-25 20:20:57 +0800

    springboot打包报错解决

---
+++ test0101/src/main/java/org/xichuang/Chapter012Application.java
@@ -0,0 +1,34 @@
+package org.xichuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter012Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter012Application.class, args);
+        System.out.println("ni da jun");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
