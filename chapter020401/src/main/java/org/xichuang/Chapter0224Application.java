fb88c303dbc1178c22d2a7a2a05a88a9fcffd9d0
xichuanwushao <avatarwx@163.com>
2020-01-25 19:31:59 +0800

    web容器配置和https

---
+++ chapter020401/src/main/java/org/xichuang/Chapter0224Application.java
@@ -0,0 +1,14 @@
+package org.xichuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter0224Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Chapter0224Application.class, args);
+    }
+
+}
---
+++ chapter020401/src/test/java/org/xichuang/chapter0224/Chapter0224ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuang.chapter0224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter0224ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
