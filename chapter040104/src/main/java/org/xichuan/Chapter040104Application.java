95634f65cba93bb4e1b918bb6cacb6ba70964e94
xichuanwushao <avatarwx@163.com>
2020-01-26 00:41:59 +0800

    返回json数据
    使用fastjson,实现WebMvcAutoConfiguration

---
+++ chapter040104/src/main/java/org/xichuan/Chapter040104Application.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter040104Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter040104Application.class, args);
+    }
+
+}
