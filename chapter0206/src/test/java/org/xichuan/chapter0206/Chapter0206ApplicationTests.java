169372f5883d95b7862fba2509b12c6c410397ee
xichuanwushao <avatarwx@163.com>
2020-01-25 19:01:06 +0800

    properties配置

---
+++ chapter0206/src/test/java/org/xichuan/chapter0206/Chapter0206ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuan.chapter0206;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter0206ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
