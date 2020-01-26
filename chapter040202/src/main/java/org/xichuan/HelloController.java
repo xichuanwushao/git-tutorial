6cfa17cc8e5595930785bc47e7f24e0351ad5543
xichuanwushao <avatarwx@163.com>
2020-01-26 12:18:41 +0800

    静态资源访问
    自定义策略，使用配置文件

---
+++ chapter040202/src/main/java/org/xichuan/HelloController.java
@@ -0,0 +1,27 @@
+package org.xichuan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+//http://localhost:8081/chapter040202/static/p1.png
+@RestController
+//http://localhost:8081/chapter02/hello
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot!";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
+
+    @GetMapping("/hello2")
+    public String hello(HttpSession session) {
+       int maxInactiveInterval = session.getMaxInactiveInterval();
+       return String.valueOf(maxInactiveInterval);
+        //return "hello https!";
+    }
+}
