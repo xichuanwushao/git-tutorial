f4be4f8fb48e42d458adf0ae07c53d0e609d6936
xichuanwushao <avatarwx@163.com>
2020-01-27 17:49:08 +0800

    JdbcTemple配置多数据源
    JdbcTemple配置多数据源

---
+++ chapter050401/src/main/java/org/xichuan/Chapter050401Application.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter050401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter050401Application.class, args);
+    }
+
+}
