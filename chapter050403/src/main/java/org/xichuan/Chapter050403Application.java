d1cb6f430684ab91e0756653f4ef3645c7825e5a
xichuanwushao <avatarwx@163.com>
2020-01-28 11:23:15 +0800

    jpa(hibernate)多数据源
    jpa(hibernate)多数据源

---
+++ chapter050403/src/main/java/org/xichuan/Chapter050403Application.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter050403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter050403Application.class, args);
+    }
+
+}
---
+++ chapter050403/src/test/java/org/xichuan/Chapter050403ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter050403ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
