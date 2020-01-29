1dfe5e98037af2b4d84af9f65a136ade43464f69
xichuanwushao <avatarwx@163.com>
2020-01-29 14:36:14 +0800

    跨域测试项目UI

---
+++ chapter040603/src/main/java/org/xichuan/Chapter040603Application.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter040603Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter040603Application.class, args);
+    }
+
+}
---
+++ chapter040603/src/test/java/org/xichuan/Chapter040603ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter040603ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
