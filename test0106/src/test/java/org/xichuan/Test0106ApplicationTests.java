7b34c3e5162b1e9970e040109e0e2b28572d598b
xichuanwushao <avatarwx@163.com>
2020-02-07 00:09:22 +0800

    springboot读取本地包

---
+++ test0106/src/test/java/org/xichuan/Test0106ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0106ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
