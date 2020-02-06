a8438d85b4b78c9c8ee679d4c0ce4108d4c4d3ce
xichuanwushao <avatarwx@163.com>
2020-02-07 00:04:16 +0800

    springboot读取xml配置

---
+++ test0107/src/main/java/org/xichuan/Test0107Application.java
@@ -0,0 +1,15 @@
+package org.xichuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+
+@SpringBootApplication
+public class Test0107Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0107Application.class, args);
+    }
+
+}
