93ed0556633cc462bd43a05e6b75305e6c47e5da
xichuanwushao <avatarwx@163.com>
2020-02-17 21:14:45 +0800

    定义proto文件，生成代码

---
+++ test0110-grpc-lib/src/main/java/org/xichuan/test0110grpclib/Test0110GrpcLibApplication.java
@@ -0,0 +1,13 @@
+package org.xichuan.test0110grpclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0110GrpcLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0110GrpcLibApplication.class, args);
+    }
+
+}
---
+++ test0110-grpc-lib/src/test/java/org/xichuan/test0110grpclib/Test0110GrpcLibApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xichuan.test0110grpclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0110GrpcLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
