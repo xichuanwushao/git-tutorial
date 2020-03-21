0a468e97e9106c7e0d919fdbdfed9c69c1696edb
xichuanwushao <avatarwx@163.com>
2020-03-21 16:33:24 +0800

    java访问ActiveMQ示例

---
+++ chapter120102/src/main/java/org/xichuan/producter.java
@@ -0,0 +1,15 @@
+package org.xichuan;
+
+/**
+ * 所属项目：chapter120102
+ * 类名称：producter
+ * 类作用：
+ * 类作者：Administrator
+ * 创建日期：2020/3/21、16:17
+ * 审核人：
+ * 审核日期：
+ * 更新记录：
+ * 其它备注：
+ */
+public class producter {
+}
